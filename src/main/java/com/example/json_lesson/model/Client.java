c8a0bff98eea462f1e8214e931b29fc7cfe3ad5b
Oleg.Avdeenko <93432136+Oleg1985Avdeenko@users.noreply.github.com>
2023-08-17 22:17:20 +0300

    Json_practice

---
+++ src/main/java/com/example/json_lesson/model/Client.java
@@ -0,0 +1,50 @@
+package com.example.json_lesson.model;
+
+public class Client {
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/test/java/com/example/json_lesson/JsonLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.json_lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonLessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
